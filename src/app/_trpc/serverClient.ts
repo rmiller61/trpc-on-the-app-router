fdb68e698cb6f39c3902c2adef6afe4927361966
Ryan <ryanmiller732@gmail.com>
2023-11-01 19:42:51 -0400

    init todos

--- src/app/_trpc/serverClient.ts
+++ src/app/_trpc/serverClient.ts
@@ -2,10 +2,16 @@ import { httpBatchLink } from "@trpc/client";
 
 import { appRouter } from "@/server";
 
+const getBaseUrl = () => {
+  if (typeof window !== "undefined") return "" // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+}
+
 export const serverClient = appRouter.createCaller({
   links: [
     httpBatchLink({
-      url: "http://localhost:3000/api/trpc",
+      url: getBaseUrl() + "/api/trpc",
     }),
   ],
 });
