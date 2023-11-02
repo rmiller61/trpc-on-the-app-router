5d3c02106a076604f4638a58e9ef72eb671582dd
Ryan <ryanmiller732@gmail.com>
2023-11-02 07:49:43 -0400

    no db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import { serverClient } from "./_trpc/serverClient";
-
+import { Suspense } from "react";
 import TodoList from "./_components/TodoList";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const todos = await serverClient.getTodos();
+  const serverRes = await serverClient.getResponse("Hello Friend");
   return (
     <main className="max-w-3xl mx-auto mt-5">
-      <TodoList initialTodos={todos} />
+      <h1 className="text-3xl font-bold">{serverRes.message}</h1>
+      <Suspense><TodoList /></Suspense>
     </main>
   );
 }
