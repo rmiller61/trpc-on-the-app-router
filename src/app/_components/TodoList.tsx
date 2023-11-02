00f9d1cc4ae6619f7fbef9af25dfc097c4473ecd
Ryan <ryanmiller732@gmail.com>
2023-11-02 10:06:14 -0400

    initialdata prop

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 
 import { trpc } from "../_trpc/client";
 
-export default function TodoList() {
+export default function TodoList({initialData}: {initialData?: any}) {
   const {data} = trpc.getListings.useQuery(undefined, {
     refetchOnMount: false,
     refetchOnReconnect: false,
     suspense: true,
+    initialData
   });
 
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,12 @@ import TodoList from "./_components/TodoList";
 
 export default async function Home() {
   const serverRes = await serverClient.getResponse("Hello Friend");
+  const serverRes2 = await serverClient.getListings();
+  
   return (
     <main className="max-w-3xl mx-auto mt-5">
       <h1 className="text-3xl font-bold">{serverRes.message}</h1>
-      <Suspense><TodoList /></Suspense>
+      <Suspense><TodoList initialData={serverRes2} /></Suspense>
     </main>
   );
 }
